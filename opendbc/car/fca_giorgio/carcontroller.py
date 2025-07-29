2b5f89961e1058e84b13a1d038e92ec9fd5cc41b
davkg <davkg@users.noreply.github.com>
2025-07-29 17:09:09 -0500

    adjust button test

--- opendbc/car/fca_giorgio/carcontroller.py
+++ opendbc/car/fca_giorgio/carcontroller.py
@@ -25,25 +25,12 @@ class CarController(CarControllerBase):
     can_sends = []
 
     # **** Button Test ****************************************************** #
-    # Testing if buttons work
-    # Send CANCEL button 1 seconds after HIGHWAY_ASSIST button is pressed
+    # Cancel spam test. Currently not working, car doesn't respond to OP button signals.
+    # Spamming too aggressively (every frame) sometimes causes panda CAN error.
 
-    highway_assist_pressed = CS.highway_assist_button > 0
-
-    # Detect rising edge of highway assist button
-    if highway_assist_pressed and not self.highway_assist_pressed_last:
-      self.cancel_button_send_frame = self.frame + 101 # 101 to land on 50Hz
-      self.cancel_button_end_frame = self.frame + 501 # Hold button for 5 seconds
-
-    if (
-      self.frame >= self.cancel_button_send_frame and
-      # (self.frame - self.cancel_button_send_frame) % 2 == 0 and
-      self.frame < self.cancel_button_end_frame
-    ):
+    if CC.cruiseControl.cancel and self.frame % self.CCP.ACC_BUTTON_STEP == 0:
       can_sends.append(fca_giorgiocan.create_acc_button_control(self.packer_pt, CANBUS.pt, CS.button_counter, cancel_button=True))
-      can_sends.append(fca_giorgiocan.create_acc_button_control(self.packer_pt, 2, CS.button_counter, cancel_button=True))
-
-    self.highway_assist_pressed_last = highway_assist_pressed
+      can_sends.append(fca_giorgiocan.create_acc_button_control(self.packer_pt, CANBUS.cam, CS.button_counter, cancel_button=True))
 
     # **** Steering Controls ************************************************ #
 
