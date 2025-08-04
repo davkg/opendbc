bf339f850db106bd84a295f6e36c55cb04b4bb15
davkg <davkg@users.noreply.github.com>
2025-08-04 12:07:18 -0500

    Revert "remvoe button from carcontroller"
    This reverts commit 676661b9573175547c947576411c4f7e587aa89e.

--- opendbc/car/fca_giorgio/carcontroller.py
+++ opendbc/car/fca_giorgio/carcontroller.py
@@ -13,9 +13,75 @@ class CarController(CarControllerBase):
     self.apply_torque_last = 0
     self.frame = 0
 
+    # Button tracking
+    self.highway_assist_pressed_last = False
+    self.acc_distance_pressed_last = False
+    self.cancel_button_send_frame = -1
+    self.cancel_button_end_frame = -1
+
+    self.button_frame = -999999
+
+
   def update(self, CC, CS, now_nanos):
     actuators = CC.actuators
     can_sends = []
+
+    # **** Button Test ****************************************************** #
+    # Send cancel button 1 second after highway assist button is detected
+    # This does not work, car doesn't respond to the cancel signal
+    # TODO: Figure out why this doesn't work
+
+    highway_assist_pressed = CS.highway_assist_button > 0
+
+    # Detect rising edge of highway assist button
+    if highway_assist_pressed and not self.highway_assist_pressed_last:
+      self.button_frame = self.frame
+
+    self.highway_assist_pressed_last = highway_assist_pressed
+
+    # Test 1: send cancel button at 50Hz on same frame as car
+    if (
+      self.frame >= self.button_frame + 100 and
+      (self.frame - self.button_frame) % 2 == 1 and # 50Hz, same frame as car
+      self.frame < self.button_frame + 400
+    ):
+      can_sends.append(fca_giorgiocan.create_acc_button_control(self.packer_pt, CANBUS.pt, CS.button_counter, cancel_button=True))
+      can_sends.append(fca_giorgiocan.create_acc_button_control(self.packer_pt, 2, CS.button_counter, cancel_button=True))
+
+    # Test 2: send cancel button at 50Hz, one frame earlier than car
+    if (
+      self.frame >= self.button_frame + 700 and
+      (self.frame - self.button_frame) % 2 == 0 and # 50Hz, one frame earlier than car
+      self.frame < self.button_frame + 1000
+    ):
+      can_sends.append(fca_giorgiocan.create_acc_button_control(self.packer_pt, CANBUS.pt, CS.button_counter, cancel_button=True))
+      can_sends.append(fca_giorgiocan.create_acc_button_control(self.packer_pt, 2, CS.button_counter, cancel_button=True))
+
+    # Test 3: send cancel button at 50Hz, same frame as car, with burst of 25 messages (Hyundai strategy)
+    if (
+      self.frame >= self.button_frame + 1300 and
+      (self.frame - self.button_frame) % 2 == 1 and
+      self.frame < self.button_frame + 1600
+    ):
+      can_sends.extend([fca_giorgiocan.create_acc_button_control(self.packer_pt, CANBUS.pt, CS.button_counter, cancel_button=True)] * 25)
+      can_sends.extend([fca_giorgiocan.create_acc_button_control(self.packer_pt, 2, CS.button_counter, cancel_button=True)] * 25)
+
+    # Test 4: send cancel button at 50Hz, one frame earlier than car, with burst of 25 messages (Hyundai strategy)
+    if (
+      self.frame >= self.button_frame + 1900 and
+      (self.frame - self.button_frame) % 2 == 0 and
+      self.frame < self.button_frame + 2200
+    ):
+      can_sends.extend([fca_giorgiocan.create_acc_button_control(self.packer_pt, CANBUS.pt, CS.button_counter, cancel_button=True)] * 25)
+      can_sends.extend([fca_giorgiocan.create_acc_button_control(self.packer_pt, 2, CS.button_counter, cancel_button=True)] * 25)
+
+    # Test 5: send cancel button at 100Hz
+    if (
+      self.frame >= self.button_frame + 2500 and
+      self.frame < self.button_frame + 2800
+    ):
+      can_sends.extend([fca_giorgiocan.create_acc_button_control(self.packer_pt, CANBUS.pt, CS.button_counter, cancel_button=True)] * 25)
+      can_sends.extend([fca_giorgiocan.create_acc_button_control(self.packer_pt, 2, CS.button_counter, cancel_button=True)] * 25)
 
     # **** Steering Controls ************************************************ #
 
