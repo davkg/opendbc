c092b2571e724ed77d7893576d785197af6c3d4d
davkg <davkg@users.noreply.github.com>
2026-08-17 13:29:41 -0500

    fix braking for civic hybrid

--- opendbc/car/honda/hondacan.py
+++ opendbc/car/honda/hondacan.py
@@ -96,7 +96,7 @@ def create_acc_commands(packer, CAN, enabled, active, accel, gas, stopping_count
   if CP.flags & HondaFlags.BOSCH_RADARLESS:
     acc_control_values.update({
       "CONTROL_ON": enabled,
-      "IDLESTOP_ALLOW": stopping_counter > 200,  # allow idle stop after 4 seconds (50 Hz)
+      "IDLESTOP_ALLOW": braking, # Enable braking for Civic Hybrid
     })
   else:
     acc_control_values.update({
